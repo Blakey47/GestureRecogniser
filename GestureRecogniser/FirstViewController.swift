82039522b8af3df98d976906f12c7cfd4fe599e6
Darragh Blake <darragh.blake48@gmail.com>
2017-10-08 18:52:51 +0100

    Added First Gesture

--- GestureRecogniser/FirstViewController.swift
+++ GestureRecogniser/FirstViewController.swift
@@ -9,15 +9,24 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    var squareView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        squareView = UIView(frame: CGRect(x: 150, y: 250, width: 100, height: 100))
+        squareView.backgroundColor = .blue
+        view.addSubview(squareView)
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
+        
+        squareView.addGestureRecognizer(panGesture)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func handlePan(recognizer: UIPanGestureRecognizer) {
+        let translationInView = recognizer.translation(in: self.view)
+        recognizer.view!.center = CGPoint(x:view.center.x + translationInView.x, y:view.center.y + translationInView.y)
     }
 
 
